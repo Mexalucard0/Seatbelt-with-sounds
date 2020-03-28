7489ab0f2ccec7d6771f7da5f27516a1f38eac7d
Mobius1 <mobius1@gmx.com>
2020-03-28 14:52:52 +0000

    Changing warning sound and icon

--- InteractSoundS/fxmanifest.lua
+++ InteractSoundS/fxmanifest.lua
@@ -15,7 +15,7 @@ ui_page('client/html/index.html')
 -- DON'T FORGET TO ADD THE SOUND FILES TO THIS!
 files({
     'client/html/index.html',
-	'client/html/sounds/seatbelt.ogg',
+	'client/html/sounds/warning.ogg',
 	'client/html/sounds/buckle.ogg',
 	'client/html/sounds/unbuckle.ogg'
 })
